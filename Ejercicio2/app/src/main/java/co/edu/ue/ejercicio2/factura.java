2f88100ba28312f2f8dad2295a1df898eaf83b43
Felix_Zamora <109540098+FelixZamora@users.noreply.github.com>
2023-11-12 23:21:23 -0500

    Add files via upload

---
+++ Ejercicio2/app/src/main/java/co/edu/ue/ejercicio2/factura.java
@@ -0,0 +1,14 @@
+package co.edu.ue.ejercicio2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class factura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factura);
+    }
+}
